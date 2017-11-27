9dc614985b336a6907f19b76188f8192c41d8a51
ejke <eikejraielo@gmail.com>
2017-11-27 10:04:03 +0000

    Update two.js
    fixes:
    * if both human and pc wins, then put human first - because she was first to win.
    * if clicking on existing x or o, disable action with if statement.

--- two.js
+++ two.js
@@ -16,48 +16,51 @@ document.addEventListener("DOMContentLoaded", function(event){
       numberofClicks++;
       // console.log("numberofClicks " +numberofClicks);
       // human adds x:
-      this.classList.add("x");
-      // console.log("you clicked item " +rightIndex+ "!");
-      // pc finds random number, attaches it to element and loops until finds element that doesn't have a class. then adds o:
-      do {
-        random = Math.floor((Math.random() * (9 - 0) + 1));
-        randomItem = document.getElementById(random);
-      } while (randomItem.classList.contains("x") && numberofClicks < 5 || randomItem.classList.contains("o") && numberofClicks < 5 || random == rightIndex);
-      randomItem.classList.add("o");
-      // console.log("computer chose " +random+ "!");
-
-      var winO = items[0].classList.contains("o") && items[1].classList.contains("o") && items[2].classList.contains("o") ||
-                 items[3].classList.contains("o") && items[4].classList.contains("o") && items[5].classList.contains("o") ||
-                 items[6].classList.contains("o") && items[7].classList.contains("o") && items[8].classList.contains("o") ||
-                 items[0].classList.contains("o") && items[3].classList.contains("o") && items[6].classList.contains("o") ||
-                 items[1].classList.contains("o") && items[4].classList.contains("o") && items[7].classList.contains("o") ||
-                 items[2].classList.contains("o") && items[5].classList.contains("o") && items[8].classList.contains("o") ||
-                 items[0].classList.contains("o") && items[4].classList.contains("o") && items[8].classList.contains("o") ||
-                 items[2].classList.contains("o") && items[4].classList.contains("o") && items[6].classList.contains("o");
-
-      var winX = items[0].classList.contains("x") && items[1].classList.contains("x") && items[2].classList.contains("x") ||
-                 items[3].classList.contains("x") && items[4].classList.contains("x") && items[5].classList.contains("x") ||
-                 items[6].classList.contains("x") && items[7].classList.contains("x") && items[8].classList.contains("x") ||
-                 items[0].classList.contains("x") && items[3].classList.contains("x") && items[6].classList.contains("x") ||
-                 items[1].classList.contains("x") && items[4].classList.contains("x") && items[7].classList.contains("x") ||
-                 items[2].classList.contains("x") && items[5].classList.contains("x") && items[8].classList.contains("x") ||
-                 items[0].classList.contains("x") && items[4].classList.contains("x") && items[8].classList.contains("x") ||
-                 items[2].classList.contains("x") && items[4].classList.contains("x") && items[6].classList.contains("x");
-
-      if (winO || winX) {
-        console.log('game over');
-        tictactoe.classList.add("win");
-        if (winO) {
-          console.log('PC win!');
-          h1.innerHTML = "PC wins!"
-        } else {
-          console.log('Human win!');
-          h1.innerHTML = "Human wins!"
-        }
+      if (this.classList.contains("x") || this.classList.contains("o")) {
+        console.log("skip, wrong item to click");
       } else {
-        console.log('game is on');
-      }
+        this.classList.add("x");
+        // console.log("you clicked item " +rightIndex+ "!");
+        // pc finds random number, attaches it to element and loops until finds element that doesn't have a class. then adds o:
+        do {
+          random = Math.floor((Math.random() * (9 - 0) + 1));
+          randomItem = document.getElementById(random);
+        } while (randomItem.classList.contains("x") && numberofClicks < 5 || randomItem.classList.contains("o") && numberofClicks < 5 || random == rightIndex);
+        randomItem.classList.add("o");
+        // console.log("computer chose " +random+ "!");
 
+        var winO = items[0].classList.contains("o") && items[1].classList.contains("o") && items[2].classList.contains("o") ||
+                   items[3].classList.contains("o") && items[4].classList.contains("o") && items[5].classList.contains("o") ||
+                   items[6].classList.contains("o") && items[7].classList.contains("o") && items[8].classList.contains("o") ||
+                   items[0].classList.contains("o") && items[3].classList.contains("o") && items[6].classList.contains("o") ||
+                   items[1].classList.contains("o") && items[4].classList.contains("o") && items[7].classList.contains("o") ||
+                   items[2].classList.contains("o") && items[5].classList.contains("o") && items[8].classList.contains("o") ||
+                   items[0].classList.contains("o") && items[4].classList.contains("o") && items[8].classList.contains("o") ||
+                   items[2].classList.contains("o") && items[4].classList.contains("o") && items[6].classList.contains("o");
+
+        var winX = items[0].classList.contains("x") && items[1].classList.contains("x") && items[2].classList.contains("x") ||
+                   items[3].classList.contains("x") && items[4].classList.contains("x") && items[5].classList.contains("x") ||
+                   items[6].classList.contains("x") && items[7].classList.contains("x") && items[8].classList.contains("x") ||
+                   items[0].classList.contains("x") && items[3].classList.contains("x") && items[6].classList.contains("x") ||
+                   items[1].classList.contains("x") && items[4].classList.contains("x") && items[7].classList.contains("x") ||
+                   items[2].classList.contains("x") && items[5].classList.contains("x") && items[8].classList.contains("x") ||
+                   items[0].classList.contains("x") && items[4].classList.contains("x") && items[8].classList.contains("x") ||
+                   items[2].classList.contains("x") && items[4].classList.contains("x") && items[6].classList.contains("x");
+
+        if (winO || winX) {
+          console.log('game over');
+          tictactoe.classList.add("win");
+          if (winX) {
+            console.log('Human win!');
+            h1.innerHTML = "Human wins!"
+          } else if (winO) {
+            console.log('PC win!');
+            h1.innerHTML = "PC wins!"
+          }
+        } else {
+          console.log('game is on');
+        }
+      }
 
     });
   });
